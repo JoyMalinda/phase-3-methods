63142529637a5f476ba628352bb7a3ac382f853b
JoyMalinda <kokimalinda@gmail.com>
2023-05-17 15:10:42 +0300

    completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"  
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+greet("Jimmy")
+
+
+def greet_with_default(name = "programmer") 
+  puts "Hello, #{name}!"
+end
+greet_with_default
+greet_with_default("Sunny")
+
+
+def add(num1,num2)
+    return num1 + num2
+end
+sum=add(2, 5)
+
+
+
+def halve(num)
+     if num.class != Integer
+         return nil
+ end
+    num/2
+ end
+my_halve = halve(6)
+my_other_halve = halve("six")
+
